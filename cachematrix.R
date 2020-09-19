d861f3329be8f2555cf68968a32d6b1ee3e4b8e1
entropybill <52928994+entropybill@users.noreply.github.com>
2020-09-19 19:07:54 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv = NULL
+  set = function(y){
+    x <<- y
+    ivs <<- NULL
+  }
+  get <-function(){x}
+  setinverse <- function(inverse){ivs <<- inverse}
+  getinverse <- function(){ivs}
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 
 }
 
@@ -11,5 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        ivs <- x$getinverse()
+  if(!is.null(ivs)){
+    message("getting cached data")
+    return(ivs)
+  }
+  data <x$get()
+  inv<- solve(data,...)
+  x$setinverse(ivs)
+  ivs
         ## Return a matrix that is the inverse of 'x'
 }
